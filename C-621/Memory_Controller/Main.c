10569d535253be977aa266523dd5ce26d4efbe07
Atandrila Anuva <anuva@Atandrilas-MacBook-Pro.local>
2021-11-01 17:48:07 -0400

    proj-2 bank conflict code

--- C-621/Memory_Controller/Main.c
+++ C-621/Memory_Controller/Main.c
@@ -15,28 +15,59 @@ int main(int argc, const char *argv[])
     if (argc != 2)
     {
         printf("Usage: %s %s\n", argv[0], "<mem-file>");
-
         return 0;
     }
 
     // Initialize a CPU trace parser
     TraceParser *mem_trace = initTraceParser(argv[1]);
+    long int size = 0;
+    
+	char c;
+    FILE* fp = fopen(argv[1], "rb");
+    for (c = getc(fp) ; c != EOF ; c = getc(fp)){
+        if (c == '\n'){ // Increment count if this character is newline
+            size++;
+        }
+    }
+    // closing the file
+    fclose(fp);
+    
+	printf("Number of Lines: %ld\n", size);
 
     // Initialize a Controller
     Controller *controller = initController();
-    // printf("%u\n", controller->bank_shift);
-    // printf("%u\n", controller->bank_mask);
+	printf("Passes Controller\n");
+    //printf("%u\n", controller->bank_shift);
+    //printf("%lu\n", controller->bank_mask);
 
     uint64_t cycles = 0;
+    uint64_t counter = 0;
+    uint64_t conflicts = 0;
+    int arr[size*1000];
+
+	//printf("Before WHILE loop\n");
 
     bool stall = false;
     bool end = false;
 
-    while (!end || ongoingPendingRequests(controller))
+	int count = 0;
+	int prev_bank = 0;
+
+	while (!end || ongoingPendingRequests(controller))
     {
+		//printf("I got in the WHILE loop\n");
         if (!end && !stall)
         {
+			//printf("I got in the 1st IF\n");
+
             end = !(getRequest(mem_trace));
+			//printf("%d\n", mem_trace->cur_req->bank_id);
+			if (mem_trace->cur_req->bank_id != 0 && mem_trace->cur_req->bank_id == prev_bank) {
+				conflicts++;
+			}
+			prev_bank = mem_trace->cur_req->bank_id;
+			//printf("Got passed CONFLICT\n");
+            //size++;
         }
 
         if (!end)
@@ -46,11 +77,15 @@ int main(int argc, const char *argv[])
 
         tick(controller);
         ++cycles;
+		count++;
     }
+
 
     free(controller->bank_status);
     free(controller->waiting_queue);
     free(controller->pending_queue);
     free(controller);
+	printf("Iteration: %d\n", count);
+    printf("Total Bank Conflicts: ""%"PRIu64"\n", conflicts);
     printf("End Execution Time: ""%"PRIu64"\n", cycles);
 }
